7cd90d5991b570b33ba931168d4faf7db82377c3
Nick B <ngbaker99@gmail.com>
2023-04-12 16:44:19 +0100

    added DB connection

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConn');
+
+// attempt to connect to MongoDB
+connectDB();
 
 const PORT = process.env.PORT || 3500;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// once connected to MongoDB start the server
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
