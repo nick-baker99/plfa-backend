55407f83c0c8bf3a2c9e2f0f18e613bb899d8991
Nick B <ngbaker99@gmail.com>
2024-02-08 17:10:56 +0000

    removed so unnecessary imports from messages route

--- routes/chatrooms/messages.js
+++ routes/chatrooms/messages.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const messagesController = require('../../controllers/messagesController');
-const verifyRoles = require('../../middleware/verifyRoles');
-const ROLES_LIST = require('../../config/roles_list');
 
 router.route('/')
   .post(messagesController.createNewMessage)
